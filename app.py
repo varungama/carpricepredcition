a2b5ff3f5b6de12fecc72aaa89afbe072e605661
varungama <90402985+varungama@users.noreply.github.com>
2021-10-20 17:52:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 import requests
 import pickle
+import os
 import pandas as pd
 import numpy as np
 import sklearn
@@ -49,5 +50,6 @@ def predict():
     else:
         return render_template('index.html')
 
-if __name__=="__main__":
-    app.run(debug=True)
+if __name__ == "__main__":
+   port = int(os.environ.get('PORT' , 5000))
+   app.run(host='0.0.0.0', port=port)
